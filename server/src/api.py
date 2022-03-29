abdf5111d8543c1cf465e4a2233aa7ba6891c494
nathanmarzzz <nathanbasketball123@gmail.com>
2022-03-28 23:32:10 -0700

    wordle and wordscape suggestions

---
+++ server/src/api.py
@@ -0,0 +1,70 @@
+from flask import Flask
+import json, os
+from dotenv import load_dotenv
+import http.client
+from urllib.parse import urlencode
+
+
+load_dotenv() # load .env into os with export probly
+API_KEY = os.environ.get('words_api_key')
+
+''' for words api ''' 
+conn = http.client.HTTPSConnection("wordsapiv1.p.rapidapi.com")
+param = {'regex': ''} 
+headers = {
+        'X-RapidAPI-Host': "wordsapiv1.p.rapidapi.com",
+        'X-RapidAPI-Key': API_KEY
+}
+numbers = ''.join([str(i) for i in range(10)])
+print('nums: ',numbers)
+app = Flask(__name__)
+
+def getWords(param):
+    pattern = urlencode(param).split("=").pop()
+    url = f'/words/?letterPattern={pattern}&lettersmin={5}&limit=100&page=1'
+
+    conn.request(
+        'GET',
+        url,
+        headers=headers
+    )
+
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    code = res.getcode()
+    return data, code
+
+
+
+'''create first route'''
+@app.route('/wordle/<letters>')
+def getWordleWords(letters):
+    param['regex'] = '^[^' + letters + numbers + " "+ ']{5}$'
+    resp, code = getWords(param)
+    
+    resp = json.loads(resp)
+    print('resp: ', resp)
+    words = resp["results"]["data"]
+
+    return {
+        'wordle': 'from wordle api', 
+        'letters to exclude': letters,
+        "words": words,
+        "code": code
+        }
+
+
+@app.route('/wordscape/<letters>')
+def getWorsdcapeWords(letters):
+    param['regex'] = '^[' + letters + '][^' + numbers + ']{3,5}$'
+    resp, code = getWords(param)
+    resp = json.loads(resp)
+    print('resp: ', resp)
+
+    words = resp["results"]["data"]
+    return {
+        'wordscape': 'from wordscape api',
+        'letters to include': letters,
+        "words": words,
+        "code": code
+    }
--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import WordList from './components/wordList';
+import GameForm from './components/GameForm';
+
 
 function App() {
+  const [game, setGame] = useState("select one");
+  const [words, setWords] = useState(null);
+  const [excludeLetters, setExclude] = useState('');
+  const [includeLetters, setInclude] = useState('');
+
+  useEffect(() => {
+    if(game === 'select one'){
+      console.log('returning... game selected is defualt value');
+      return;
+    }
+
+    const letters = game === 'wordscape' ? includeLetters : excludeLetters; 
+    if(!letters){
+      return;
+    }
+    console.log('sedning: ',letters)
+    fetch(`/${game}/${letters}`)
+      .then(resp => resp.json())
+      .then(res => {
+        console.log('res from api: ', res);
+        setWords(res['words']);
+      });
+
+  },[game, excludeLetters, includeLetters]);
+
+  // from wordle
+  const setExcludeLetters = (excludeLetters) => {
+    console.log('setting exclude: ', excludeLetters);
+    setExclude(excludeLetters)
+  }
+
+  // will be from wordscape 
+  const setIncludeLetters = (includeLetters) => {
+    console.log('setting: include: ', includeLetters);
+    setInclude(includeLetters)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Find the word you're looking for
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div >
+          <label htmlFor="game-select"> chose game</label>
+          <select id="game-select" onChange={(e) => setGame(e.target.value)}>
+            <option value="select one">select one</option>
+            <option value="wordle">wordle</option>
+            <option value="wordscape">wordscape</option>
+          </select>
+          <p> selected value is: {game}</p>
+          {
+            game !== "select one" ?
+            (
+              <div className='gameForm'>
+                <GameForm 
+                  game={game}
+                  setExclude={setExcludeLetters}
+                  setInclude={setIncludeLetters}
+                ></GameForm>
+            </div>
+            ) : null
+          }
+          {
+            words ? 
+            (
+              <div className='api-results'>
+                <WordList words={words}></WordList>
+              </div>
+            ) : null
+          }
+        </div>
       </header>
     </div>
   );
